cb85e7039d3992ac3ae5306d988caabae9fac542
Alexander Simmons <41705368+AlexSimmonsFCC@users.noreply.github.com>
2018-09-28 12:43:56 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -81,23 +81,23 @@ require([
       }
 
       // Create feature layers
-      var fOneOrMore = new TileLayer({
+      var fOneOrMore = new VectorTileLayer({
         url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_OneOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
         outFields: ['*']
       })
 
-     var fTwoOrMore = new TileLayer({
+     var fTwoOrMore = new VectorTileLayer({
         url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_TwoOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
         outFields: ['*']
       })
 
-     var fThreeOrMore = new TileLayer({
+     var fThreeOrMore = new VectorTileLayer({
         url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_ThreeOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
         outFields: ['*']
       })
 
 
-    var fFourOrMore = new TileLayer({
+    var fFourOrMore = new VectorTileLayer({
         url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_FourOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
         outFields: ['*']
       })
