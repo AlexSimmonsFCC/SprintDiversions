7d9a1bf74b4d48d2a92cb34dc5ac0973526f510c
Alexander Simmons <41705368+AlexSimmonsFCC@users.noreply.github.com>
2018-12-10 16:30:02 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,162 +1,141 @@
-require([
-  'esri/Map',
-  'esri/views/MapView',
-  'esri/Basemap',
-  'esri/layers/VectorTileLayer',
-  'esri/layers/FeatureLayer',
-  'esri/layers/WebTileLayer',
-  'esri/layers/TileLayer',
-  'esri/widgets/Search',
-  'esri/widgets/Home',
-  'esri/widgets/Locate'
-],
-    function (Map, MapView, Basemap, VectorTileLayer, FeatureLayer, WebTileLayer, TileLayer, Search, Home, Locate) {
-      var map, view, searchWidget, homeBtn, locateBtn
-      var layers = window.layers
-      var featLayers = []
+ // one global for persistent app variables
+      var app = {};
+      require([
+        "esri/map",
+        "esri/layers/ArcGISTiledMapServiceLayer", "esri/layers/ArcGISDynamicMapServiceLayer",
+        "esri/request", "esri/config",
+        "esri/tasks/ClassBreaksDefinition", "esri/tasks/AlgorithmicColorRamp",
+        "esri/tasks/GenerateRendererParameters", "esri/tasks/GenerateRendererTask",
+        "esri/layers/LayerDrawingOptions",
+        "esri/symbols/SimpleFillSymbol", "esri/dijit/Legend",
+        "dojo/parser", "dojo/_base/array", "esri/Color", "dojo/dom-style",
+        "dojo/json", "dojo/dom",
+        "dojo/data/ItemFileReadStore",
+        "dijit/registry",
 
-        // Create base layer from Mapbox street layer
-      var mapBaseLayer = new WebTileLayer({
-        urlTemplate: 'https://{subDomain}.tiles.mapbox.com/v4/fcc.map-toolde8w/{level}/{col}/{row}.png?access_token=pk.eyJ1IjoiZmNjIiwiYSI6InBiaGMyLU0ifQ.LOmVYpUCFv2yWpbvxDdQNg',
-        subDomains: ['a', 'b', 'c', 'd'],
-        copyright: '\u00A9 OpenStreetMap contributors Design \u00A9 Mapbox'
-      })
+        "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dijit/form/FilteringSelect",
+        "dojo/domReady!"
+      ], function(
+        Map,
+        ArcGISTiledMapServiceLayer, ArcGISDynamicMapServiceLayer,
+        esriRequest, esriConfig,
+        ClassBreaksDefinition, AlgorithmicColorRamp,
+        GenerateRendererParameters, GenerateRendererTask,
+        LayerDrawingOptions,
+        SimpleFillSymbol, Legend,
+        parser, arrayUtils, Color, domStyle,
+        JSON, dom,
+        ItemFileReadStore,
+        registry
+      ) {
+        parser.parse();
 
-        // Create base map from Mapbox layer
-      var mapBox = new Basemap({
-        baseLayers: [mapBaseLayer],
-        title: 'Street'
-      })
+        esriConfig.defaults.io.proxyUrl = "/proxy/";
 
-        // Create map
-      map = new Map({
-        basemap: mapBox
-      })
+        app.dataUrl = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/2";
+        app.defaultFrom = "#ffffcc";
+        app.defaultTo = "#006837";
 
-        // Make map view and bind it to the map
-      view = new MapView({
-        container: 'map',
-        map: map,
-        center: [-98, 38.48],
-        zoom: 4,
-        constraints: {
-          minZoom: 4,
-          maxZoom: 9,
-          rotationEnabled: false
+        app.map = new Map("map", {
+          center: [-85.787, 39.782],
+          zoom: 6,
+          slider: false
+        });
+
+        var basemap = new ArcGISTiledMapServiceLayer("https://services.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer");
+        app.map.addLayer(basemap);
+        var ref = new ArcGISTiledMapServiceLayer("https://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Reference_Overlay/MapServer");
+        app.map.addLayer(ref);
+
+        // add US Counties as a dynamic map service layer
+        var urlDyn = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer";
+        var usaLayer = new ArcGISDynamicMapServiceLayer(urlDyn, {
+          id: "us_counties",
+          opacity: 0.7,
+          visible: false
+        });
+        usaLayer.setVisibleLayers([2]);
+        app.map.addLayer(usaLayer);
+
+        // get field info
+        var countyFields = esriRequest({
+          url: app.dataUrl,
+          content: {
+            f: "json"
+          },
+          callbackParamName: "callback"
+        });
+        countyFields.then(function(resp) {
+          var fieldNames, fieldStore;
+
+          fieldNames = { identifier: "value", label: "name", items: [] };
+          arrayUtils.forEach(resp.fields.slice(6, 16), function(f) { // add some field names to the FS
+            fieldNames.items.push({ "name": f.name, "value": f.name });
+          });
+          fieldStore = new ItemFileReadStore({ data: fieldNames });
+          registry.byId("fieldNames").set("store", fieldStore);
+          registry.byId("fieldNames").set("value", "POP2007"); // set a value
+        }, function(err) {
+          console.log("failed to get field names: ", err);
+        });
+
+        // update renderer when field name changes
+        registry.byId("fieldNames").on("change", getData);
+        registry.byId("fieldNames").set("value", "POP_2007"); // triggers getData()
+
+        function getData() {
+          classBreaks(app.defaultFrom, app.defaultTo);
         }
-      })
 
-        // Add search widget
-      searchWidget = new Search({
-        view: view
-      })
+        function classBreaks(c1, c2) {
+          var classDef = new ClassBreaksDefinition();
+          classDef.classificationField = registry.byId("fieldNames").get("value") || "POP2000";
+          classDef.classificationMethod = "natural-breaks"; // always natural breaks
+          classDef.breakCount = 5; // always five classes
 
-        // Position search widget
-      view.ui.add(searchWidget, {
-        position: 'top-right',
-        index: 2
-      })
+          var colorRamp = new AlgorithmicColorRamp();
+          colorRamp.fromColor = new Color.fromHex(c1);
+          colorRamp.toColor = new Color.fromHex(c2);
+          colorRamp.algorithm = "hsv"; // options are:  "cie-lab", "hsv", "lab-lch"
 
-        // Add Home widget
-      homeBtn = new Home({
-        view: view
-      })
+          classDef.baseSymbol = new SimpleFillSymbol("solid", null, null);
+          classDef.colorRamp = colorRamp;
 
-        // Position Home widget
-      view.ui.add(homeBtn, 'top-left')
+          var params = new GenerateRendererParameters();
+          params.classificationDefinition = classDef;
+          var generateRenderer = new GenerateRendererTask(app.dataUrl);
+          generateRenderer.execute(params, applyRenderer, errorHandler);
+        }
 
-        // Add locate widget
-      locateBtn = new Locate({
-        view: view
-      })
+        function applyRenderer(renderer) {
+          // dynamic layer stuff
+          var optionsArray = [];
+          var drawingOptions = new LayerDrawingOptions();
+          drawingOptions.renderer = renderer;
+          // set the drawing options for the relevant layer
+          // optionsArray index corresponds to layer index in the map service
+          optionsArray[2] = drawingOptions;
+          app.map.getLayer("us_counties").setLayerDrawingOptions(optionsArray);
+          app.map.getLayer("us_counties").show();
+          // create the legend if it doesn't exist
+          if ( ! app.hasOwnProperty("legend") ) {
+            createLegend();
+          }
+        }
 
-        // Position locate widget
-      view.ui.add(locateBtn, {
-        position: 'top-left'
-      })
+        function createLegend() {
+          app.legend = new Legend({
+            map : app.map,
+            layerInfos : [ {
+              layer : app.map.getLayer("us_counties"),
+              title : "US Counties"
+            } ]
+          }, dom.byId("legendDiv"));
+          app.legend.startup();
+        }
 
-      var template = {
-        title: 'caf2_auction_cam_auction_cbg_geom',
-        content: '<ul style="margin-top: 0"><li>cbg_id = {cbg_id}</li><li>stateabbr = {stateabbr}</li><li>bidder = {bidder}</li><li>tier = {tier}</li><li>latency = {latency}</li><li>locations = {locations}</li><ul>'
-      }
-
-      // Create feature layers
-      var fOneOrMore = new VectorTileLayer({
-        url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_OneOrMore_Providers_Dec2017/VectorTileServer/resources/styles/root.json?f=pjson&token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
-        outFields: ['*']
-      })
-
-
-     var fOneOrMore = new VectorTileLayer({
-        url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_OneOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
-        outFields: ['*']
-      })
-
-
-     var fTwoOrMore = new VectorTileLayer({
-        url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_TwoOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
-        outFields: ['*']
-      })
-
-     var fThreeOrMore = new VectorTileLayer({
-        url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_ThreeOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
-        outFields: ['*']
-      })
-
-
-    var fFourOrMore = new VectorTileLayer({
-        url: 'https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/F477_LTE_By_Block_FourOrMore_Providers_Dec2017/VectorTileServer?token=dls8NktxFcfUUiCiaid15zO_T-_iz491gkT5f_9v-_21sCJFaPx0GVdYTR_aP73qoR9xgy7LMCo3_zn9PyhZxyFGDiildbNDR_DbaRxQbbsUQki2IguWTiWcr-UyiSz-QRs5kGzUmMCHHgl0W40oUtxUlrOuOWBwK5In2n7mDAtGKIPPOomdPUcMeQXVKejNy_7399Ey_gircnXT14Cq_plw0maUy8Px08msKBj1VWk.',
-        outFields: ['*']
-      })
-
-
-      fLayer.popupTemplate = template
-
-        // Add tile layers to map
-      map.addMany([fOneOrMore,fTwoOrMore,fThreeOrMore,fFourOrMore])
-      
-
-        // bind radio button event
-        // var radios = document.layerControl.layerOpts;
-        // for (var i = 0, radiosLen = radios.length; i < radiosLen; i++) {
-        //     radios[i].onchange = function() {
-        //         updateLayerVisibility(this.value);
-        //     };
-        // }
-
-        // toggle layer visibility
-        // function updateLayerVisibility(indx) {
-
-        //     for (var i = 0; i < featLayers.length; i++) {
-        //         featLayers[i].visible = false;
-        //     }
-
-        //     featLayers[indx].visible = true;
-
-        // }
-
-        // set default layer to visible
-        // updateLayerVisibility(0);
-
-        // toggle legend display
-      $('#btn-closeLegend').on('click', function (e) {
-        e.preventDefault()
-        $('.map-legend').hide('fast')
-      })
-
-      $('#btn-openLegend').on('click', function (e) {
-        e.preventDefault()
-        $('.map-legend').show('fast')
-      })
-
-        // toggle layer control display
-      $('#btn-closeLayerCtrl').on('click', function (e) {
-        e.preventDefault()
-        $('.layer-control').hide('fast')
-      })
-
-      $('#btn-openLayerCtrl').on('click', function (e) {
-        e.preventDefault()
-        $('.layer-control').show('fast')
-      })
-    })
+        function errorHandler(err) {
+          // console.log("Something broke, error: ", err);
+          console.log("error: ", JSON.stringify(err));
+        }
+      });
