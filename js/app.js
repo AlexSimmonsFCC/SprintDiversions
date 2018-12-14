e3f963652d7b29b7c486dce03e53f1e5ffcb11e8
Alexander Simmons <41705368+AlexSimmonsFCC@users.noreply.github.com>
2018-12-14 14:15:31 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@
 
         esriConfig.defaults.io.proxyUrl = "/proxy/";
 
-        app.dataUrl = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/2";
+        app.dataUrl = "https://services.arcgis.com/YnOQrIGdN9JGtBh4/arcgis/rest/services/CMA_Join/FeatureServer";
         app.defaultFrom = "#ffffcc";
         app.defaultTo = "#006837";
 
