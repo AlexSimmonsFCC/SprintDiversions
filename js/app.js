d93eca71fef5463d6152433a8580eb775472cf8d
Alexander Simmons <41705368+AlexSimmonsFCC@users.noreply.github.com>
2018-12-17 11:30:34 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@
 
      esriConfig.defaults.io.proxyUrl = "/proxy/";
 
-        app.dataUrl = "https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/CMA_MapService/MapServer/0";
+        app.dataUrl = "https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/CMA_Service/MapServer/0";
         app.defaultFrom = "#ffffcc";
         app.defaultTo = "#006837";
 
@@ -48,7 +48,7 @@
         app.map.addLayer(ref);
 
         // add US Counties as a dynamic map service layer
-        var urlDyn = "https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/CMA_MapService/MapServer/";
+        var urlDyn = "https://tiles.arcgis.com/tiles/YnOQrIGdN9JGtBh4/arcgis/rest/services/CMA_Service/MapServer";
         var usaLayer = new ArcGISDynamicMapServiceLayer(urlDyn, {
           id: "us_counties",
           opacity: 0.7,
