545942bfa967002a0abe5ef181b3c762a18f8efb
Alexander Simmons <41705368+AlexSimmonsFCC@users.noreply.github.com>
2019-02-01 16:58:01 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ require([
   app.outFields = ["swdiversio", "market_pop"];
   app.currentAttribute = "market_pop";
   app.popupTemplate = new PopupTemplate({
-    title: "{MarketName} + county",
+    title: {cma} "{cma} + county",
     fieldInfos: [{
       "fieldName": app.currentAttribute,
       "label": app.fields[app.currentAttribute],
